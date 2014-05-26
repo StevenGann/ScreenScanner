fb79f73a6b5f2db63dd1ba3f55e82d4d8ed0bc52
StevenGann <sgann2012@fau.edu>
2014-05-26 18:34:34 -0400

    Added Reverse Log Sampling
    Added a reverse logarithmic distribution function for random sampling.
    It favors sampling from pixels closer to the edges of the screen.

--- ScreenScanner/ScreenScanner.cs
+++ ScreenScanner/ScreenScanner.cs
@@ -25,7 +25,9 @@ namespace ScreenScanner
 
 		//Private Fields
 		private int screenHeight;
+		private int screenHeightMid;
 		private int screenWidth;
+		private int screenWidthMid;
 		private FixedSizedQueue<Color> rgbQ;// 
 		private Timer timer1 = new Timer();
 		private Random rnd = new Random();
@@ -34,7 +36,9 @@ namespace ScreenScanner
 		public Scanner()
 		{
 			screenHeight = GetScreenHeight();
+			screenHeightMid = screenHeight / 2;
 			screenWidth = GetScreenWidth();
+			screenWidthMid = screenWidth / 2;
 
 			rgbQ = new FixedSizedQueue<Color>(50);
 
@@ -82,16 +86,45 @@ namespace ScreenScanner
 		private void timerTick(object sender, EventArgs e)
 		{
 			//To Do: Add method for switching pixel selection methods.
-            
+			int newX = screenWidthMid;
+			int newY = screenHeightMid;
+
 			//Linear Distribution
 			//Completely random.
-			//int newX = rnd.Next(screenWidth);
-			//int newY = rnd.Next(screenHeight);
+			//newX = rnd.Next(screenWidth);
+			//newY = rnd.Next(screenHeight);
 
 			//Logarithmic Distribution
 			//Random, but more likely to select from closer to the center of the screen.
-			int newX = (int)(((float)(rnd.Next(screenWidth)+rnd.Next(screenWidth)))/2.0f);
-			int newY = (int)(((float)(rnd.Next(screenHeight) + rnd.Next(screenHeight))) / 2.0f);
+			//newX = (int)(((float)(rnd.Next(screenWidth)+rnd.Next(screenWidth)))/2.0f);
+			//newY = (int)(((float)(rnd.Next(screenHeight) + rnd.Next(screenHeight))) / 2.0f);
+
+			//Reverse Logarithmic Distribution
+			//Random, but more likely to select from closer to the edge of the screen.
+			int lin1 = rnd.Next(screenWidth);
+			int lin2 = rnd.Next(screenWidth);
+			newX = (int)(((float)(lin1 + lin2)) / 2.0f);
+			if (newX > screenWidthMid)
+			{
+				newX += Math.Min(lin1, lin2);
+			}
+			else
+			{
+				newX -= Math.Min(lin1, lin2);
+			}
+
+			lin1 = rnd.Next(screenHeight);
+			lin2 = rnd.Next(screenHeight);
+			newY = (int)(((float)(lin1 + lin2)) / 2.0f);
+			if (newY > screenHeightMid)
+			{
+				newY += Math.Min(lin1, lin2);
+			}
+			else
+			{
+				newY -= Math.Min(lin1, lin2);
+			}
+
 
 			//To Do: Design selection algorithm that biases the outer portions of the screen.
 
